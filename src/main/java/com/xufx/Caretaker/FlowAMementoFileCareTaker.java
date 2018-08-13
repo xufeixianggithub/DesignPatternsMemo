834fe738386708b2be798db4d6c5f0a1c8c1cc5a
zude <zude@seeapp.com>
2018-08-13 17:15:04 +0800

    备忘录模式示例

--- src/main/java/com/xufx/Caretaker/FlowAMementoFileCareTaker.java
+++ src/main/java/com/xufx/Caretaker/FlowAMementoFileCareTaker.java
@@ -1,28 +1,30 @@
 package com.xufx.Caretaker;
 
-import com.xufx.Interfaces.Memento;
+
+
+import com.xufx.Interfaces.FlowAMockMemento;
 
 import java.util.*;
 /**
  * 负责保存备忘录的对象
  */
-public class Caretaker{
+public class FlowAMementoFileCareTaker {
     /**
      * 记录被保存的备忘录对象
      */
-    private Memento memento = null;
+    private FlowAMockMemento memento = null;
     /**
      * 保存备忘录对象
      * @param memento 被保存的备忘录对象
      */
-    public void saveMemento(Memento memento){
+    public void saveMemento(FlowAMockMemento memento){
         this.memento = memento;
     }
     /**
      * 获取被保存的备忘录对象
      * @return 被保存的备忘录对象
      */
-    public Memento retriveMemento(){
+    public FlowAMockMemento retriveMemento(){
         return this.memento;
     }
 }
---
+++ src/main/java/com/xufx/Interfaces/FlowAMockMemento.java
@@ -0,0 +1,9 @@
+package com.xufx.Interfaces;
+
+import java.io.*;
+/**
+ * 模拟运行流程A的对象的备忘录接口，是个窄接口
+ */
+public interface FlowAMockMemento extends java.io.Serializable{
+    //空的
+}
